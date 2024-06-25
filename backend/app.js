2b6c7b04da7369f6f780bfba320143eabe7b7123
Zvki1 <rzaki@hotmail.fr>
2024-06-25 23:07:12 +0100

    init

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectDB()
+  .then(() => {
+    // Start the server only if the DB connection is successful
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB. Server not started.");
+  });
