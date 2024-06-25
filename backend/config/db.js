3290e7ffdc2c8dcfe506fba80edc23b5ec7a7d7d
Zvki1 <rzaki@hotmail.fr>
2024-06-25 23:08:51 +0100

    initialise connectioon to db

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,6 +11,6 @@ const connectDB = async () => {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1); // Exit process with failure
   }
-};
+}
 
 module.exports = connectDB;
